df23e8b2136cddf63f8d0b2e0c577ea27069d0a0
Grexilo <43388194+Grexilo@users.noreply.github.com>
2022-03-11 09:24:03 -0500

    /tomaDatos

--- Toma_Datos.m
+++ Toma_Datos.m
@@ -28,20 +28,31 @@ step(1:20) = 0;
 
 t = 0:0.5:599.5;
 
-subplot(2,1,1)
+subplot(3,1,1)
 plot(t,step)
 ylim([-0.2  1.2])
 title("Entrada Escalon")
 xlabel("t (Segundos) ")
 grid on
 
-subplot(2,1,2)
+subplot(3,1,2)
 plot(t,data)
 title("Tempetura Bombillo")
 xlabel("t (segundos)")
 ylabel(" Grados (°C)")
 ylim([min(data)-5 max(data)+5])
 grid on
+
+subplot(3,1,3)
+dataFilter = movmean(data,10);
+plot(t,dataFilter)
+title("Temperatura Bombillo - Promedio movil de 10 muestras")
+xlabel("t (segundos)")
+ylabel(" Grados (°C)")
+ylim([min(data)-5 max(data)+5])
+grid on
+
+
 
 
 
