b0bcdf83e4042d064a3056456c993965d950eec6
Grexilo <43388194+Grexilo@users.noreply.github.com>
2022-03-11 09:15:33 -0500

    Add files via upload

---
+++ Toma_Datos.m
@@ -0,0 +1,47 @@
+clc; clear all;
+
+%Tiempo de espera conectar el arduino 
+%El codigo en arduino no se ejecuta hasta leer el puerto serial
+disp("ya")
+pause(3)
+
+s = serialport("COM5",115200);
+disp(s)
+
+data = [];
+
+%Estamos muestreando a 2 muestas por segundo, por pruebas observamos que la
+%bombilla tarda 10 minutos en llegar a una temperatura estable, por
+%ende, 10min* 60s * 2 muestras=1200muestras
+tic
+for k = 1:1:1200 %cantidad total de muestras
+    
+    data(k) = readline(s);
+    disp(data)
+    
+end
+toc
+%%
+
+step = ones(1,length(data));
+step(1:20) = 0;
+
+t = 0:0.5:599.5;
+
+subplot(2,1,1)
+plot(t,step)
+ylim([-0.2  1.2])
+title("Entrada Escalon")
+xlabel("t (Segundos) ")
+grid on
+
+subplot(2,1,2)
+plot(t,data)
+title("Tempetura Bombillo")
+xlabel("t (segundos)")
+ylabel(" Grados (°C)")
+ylim([min(data)-5 max(data)+5])
+grid on
+
+
+
